9b307eb546d6b398531b5a3d1e722d4cbb366153
STS-Engineer <administration.sts@avocarbon.com>
2025-12-16 09:27:19 +0100

    Update AddEmployeeModal.js

--- src/components/AddEmployeeModal.js
+++ src/components/AddEmployeeModal.js
@@ -307,13 +307,15 @@ const AddEmployeeModal = ({ isOpen, onClose, onAdd }) => {
                 value={formData.site_dep} 
                 onChange={handleInputChange}
                 options={[
-                  t('headquarters'),
-                  t('northSite'), 
-                  t('southSite'),
-                  t('eastSite'),
-                  t('westSite'),
-                  t('hrDepartment'),
-                  t('itDepartment')
+                  t('Commerce'),
+                  t('Finance'), 
+                  t('Chiffrage'),
+                  t('Digital'),
+                  t('General'),
+                  t('Logistique Germany'),
+                  t('Logistique Groupe'),
+                  t('Achat'),
+                  t('Qualité')
                 ]}
                 required 
               />
