0601255f46c6409b5a01db9664c80b48c3c51233
STS-Engineer <administration.sts@avocarbon.com>
2026-01-06 09:29:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/App.css';
 import App from './App';
+import { LanguageProvider } from './contexts/LanguageContext';
+import { NotificationProvider } from './contexts/NotificationContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <NotificationProvider>
+        <App />
+      </NotificationProvider>
+    </LanguageProvider>
   </React.StrictMode>
 );
